7a560a05820b96eb9ab9c18bed4ea2aa4fe37554
Konrad <kondzio_60@interia.pl>
2016-02-12 10:16:36 +0100

    First commit

---
+++ src/pl/balu/encoding/Test.java
@@ -0,0 +1,70 @@
+package pl.balu.encoding;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class Test {
+  
+  public static void main(String[] args) {
+    File file = new File("D:\\1.txt");
+    
+    try {
+      /*
+       * Reading a Image file from file system
+       */
+      FileInputStream inFile = new FileInputStream(file);
+      byte fileData[] = new byte[(int)file.length()];
+      inFile.read(fileData);
+      
+      /*
+       * Converting Image byte array into Base64 String 
+       */
+      String fileDataString = encodeFile(fileData);
+      
+      /*
+       * Converting a Base64 String into Image byte array 
+       */
+      byte[] fileByteArray = decodeFile(fileDataString);
+      
+      /*
+       * Write a image byte array into file system  
+       */
+      FileOutputStream outFile = new FileOutputStream("D:\\2.txt");
+      outFile.write(fileByteArray);
+      
+      inFile.close();
+      outFile.close();
+      
+      System.out.println("File Successfully Manipulated!");
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found" + e);
+    } catch (IOException ioe) {
+      System.out.println("Exception while reading the File " + ioe);
+    }
+
+  }
+  
+  /**
+   * Encodes the byte array into base64 string
+   * @param fileByteArray - byte array
+   * @return String a {@link java.lang.String}
+   */
+  public static String encodeFile(byte[] fileByteArray){    
+    return Base64.encodeBase64URLSafeString(fileByteArray);    
+  }
+  
+  /**
+   * Decodes the base64 string into byte array
+   * @param fileDataString - a {@link java.lang.String} 
+   * @return byte array
+   */
+  public static byte[] decodeFile(String fileDataString) {    
+    return Base64.decodeBase64(fileDataString);
+  }
+
+}
